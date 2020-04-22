0a1433c717aa0507274aea0a8d32b50c5b29a0f4
David Ross <daboross@daboross.net>
2020-04-22 13:03:37 -0700

    Commit raidwas's version with permission

--- benches/vec_benchmarks.rs
+++ benches/vec_benchmarks.rs
@@ -2,27 +2,17 @@ use std::time::Duration;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-const OUTER_VEC_SIZE: usize = 100_000_000;
+const OUTER_VEC_SIZE: usize = 10_000_000;
 
 fn alloc_flat() -> Vec<u16> {
     vec![0; 60 * OUTER_VEC_SIZE]
 }
 
-fn loop_flat_bad_iteration_order(v: &Vec<u16>) {
+fn loop_flat(v: &Vec<u16>) {
     let mut _counter = 0;
-    for _i in 0..OUTER_VEC_SIZE {
-        for _j in 0..60 {
-            _counter += v[_j * OUTER_VEC_SIZE + _i];
-        }
-    }
-    black_box(_counter);
-}
-
-fn loop_flat_correct_iteration_order(v: &Vec<u16>) {
-    let mut _counter = 0;
-    for _i in 0..OUTER_VEC_SIZE {
-        for _j in 0..60 {
-            _counter += v[_i * 60 + _j];
+    for i in 0..OUTER_VEC_SIZE {
+        for j in 0..60 {
+            _counter += v[i * 60 + j];
         }
     }
     black_box(_counter);
@@ -35,31 +25,34 @@ fn loop_flat_explicit_size_assert(v: &Vec<u16>) {
     assert!(v.len() >= OUTER_VEC_SIZE * 60);
 
     let mut _counter = 0;
-    for _i in 0..OUTER_VEC_SIZE {
-        for _j in 0..60 {
-            _counter += v[_i * 60 + _j];
+    for i in 0..OUTER_VEC_SIZE {
+        for j in 0..60 {
+            _counter += v[i * 60 + j];
         }
     }
     black_box(_counter);
 }
 
-fn loop_flat_iterator(v: &Vec<u16>) {
-    let mut _counter = 0;
-
-    for (idx, val) in v.iter().enumerate() {
-        // these aren't used so they'll be optimized out - they're just here to show how this
-        // style could be used practically.
-        let _i = idx / 60;
-        let _j = idx % 60;
-        _counter += val;
-    }
+//This is just to see if the compiler actually removes bound checks in the loop_flat_explicit_size_assert version
+fn loop_flat_explicit_size_assert_unchecked(v: &Vec<u16>) {
+    // This helps the compiler elide bounds checks when doing non-Iterator-based iteration:
+    // if the bounds are broken, it explicitly panics early, so it shouldn't need to test
+    // those bounds later.
+    assert!(v.len() >= OUTER_VEC_SIZE * 60);
 
     let mut _counter = 0;
-    for _i in 0..OUTER_VEC_SIZE {
-        for _j in 0..60 {
-            _counter += v[_i * 60 + _j];
+    for i in 0..OUTER_VEC_SIZE {
+        for j in 0..60 {
+            _counter += unsafe { v.get_unchecked(i * 60 + j) };
         }
     }
+    black_box(_counter);
+}
+
+fn loop_flat_functional(v: &Vec<u16>) {
+    let _counter = v
+        .chunks_exact(60)
+        .fold(0u16, |acc, chunk| chunk.iter().sum::<u16>() + acc);
     black_box(_counter);
 }
 
@@ -79,8 +72,8 @@ fn loop_nested(v: &Vec<Vec<u16>>) {
 
 fn benchmark_nested(c: &mut Criterion) {
     let mut group = c.benchmark_group(&format!("nested vec of {}", OUTER_VEC_SIZE));
-    group.sample_size(50);
-    group.measurement_time(Duration::from_secs(60 * 30));
+    group.sample_size(10);
+    group.measurement_time(Duration::from_secs(400));
 
     group.bench_function("alloc", |b| b.iter(|| alloc_nested()));
     let nested = alloc_nested();
@@ -92,22 +85,20 @@ fn benchmark_nested(c: &mut Criterion) {
 
 fn benchmark_flat(c: &mut Criterion) {
     let mut group = c.benchmark_group(&format!("flat vec of {}", OUTER_VEC_SIZE));
-    group.sample_size(50);
-    group.measurement_time(Duration::from_secs(60 * 30));
+    group.sample_size(10);
+    group.measurement_time(Duration::from_secs(400));
 
     group.bench_function("alloc", |b| b.iter(|| alloc_flat()));
     let flat = alloc_flat();
-    group.bench_function("loop with bad order", |b| {
-        b.iter(|| loop_flat_bad_iteration_order(black_box(&flat)))
-    });
-    group.bench_function("loop with good order", |b| {
-        b.iter(|| loop_flat_correct_iteration_order(black_box(&flat)))
-    });
-    group.bench_function("loop with assert, good order", |b| {
+    group.bench_function("loop", |b| b.iter(|| loop_flat(black_box(&flat))));
+    group.bench_function("loop with assert", |b| {
         b.iter(|| loop_flat_explicit_size_assert(black_box(&flat)))
     });
-    group.bench_function("loop with iterator, good order", |b| {
-        b.iter(|| loop_flat_iterator(black_box(&flat)))
+    group.bench_function("loop with assert and unchecked", |b| {
+        b.iter(|| loop_flat_explicit_size_assert_unchecked(black_box(&flat)))
+    });
+    group.bench_function("loop functional", |b| {
+        b.iter(|| loop_flat_functional(black_box(&flat)))
     });
 }
 
